e7689e2d25bc51d4a4057f2c0c196553c52f8cca
Xiaoyun Sun <hixysun@gmail.com>
2017-10-23 14:58:19 +0800

    add input from user

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,19 @@
 # hello-world.py
 
-print 'hello world.'
+import sys
+import argparse
+
+def printHello(args):
+	if len(args) == 0:
+		print 'hello world.'
+	elif len(args) == 1:
+		print 'hello world, ', args[1]
+	else:
+		print 'too many input values.'
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser('hello')
+	args = vars(parser.parse_args())
+	print 'type: ', type(args)
+	printHello(args)
